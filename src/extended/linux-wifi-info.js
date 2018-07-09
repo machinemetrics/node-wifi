8afc3afec73903df1a2be77e7d4cb90582fde400
adnanba <adnanbabovic83@gmail.com>
2018-07-09 13:43:33 +0200

    Testing

--- src/extended/linux-wifi-info.js
+++ src/extended/linux-wifi-info.js
@@ -3,23 +3,21 @@ var networkUtils = require('../network-utils');
 var env = require('../env');
 
 function getWifiInfo(config, callback) {
-  var commandStr = "nmcli -t -f ssid dev wifi  " 
-                + "&& nmcli dev show wlp1s0 | grep IP4.ADDRESS | awk '{print $2}' "
-                + "&& nmcli dev show wlp1s0 | grep GENERAL.HWADDR | awk '{print $2}' ";
+  var commandStr = "nmcli -t -f ssid dev wifi && nmcli dev show wlp1s0 | grep IP4.ADDRESS | awk '{print $2}' && nmcli dev show wlp1s0 | grep GENERAL.HWADDR | awk '{print $2}' ";
 
   exec(commandStr, env, function(err, stdOut) {
       if (err) {
         callback && callback(err);
         return;
       }
-      data = stdOut.split('\n');
-      let wifiInfo = {
-        ssid: data[0],
-        address: data[1],
-        MAC: data[2]
-      };
-
-      callback && callback(null, wifiInfo);
+      // data = stdOut.split('\n');
+      // let wifiInfo = {
+      //   ssid: data[0],
+      //   address: data[1],
+      //   MAC: data[2]
+      // };
+      console.log('Wifi Info from node-wifi', stdOut);
+      callback && callback(null, stdOut);
   });
 }
 
