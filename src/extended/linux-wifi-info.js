d1977d9078e140f4311a8af6df1512390e11b039
adnanba <adnanbabovic83@gmail.com>
2018-08-09 16:14:54 +0200

    Change order of wifi info

--- src/extended/linux-wifi-info.js
+++ src/extended/linux-wifi-info.js
@@ -3,7 +3,7 @@ var networkUtils = require('../network-utils');
 var env = require('../env');
 
 function getWifiInfo(config, callback) {
-  var commandStr = "nmcli radio wifi && nmcli dev wifi list | grep '*' | sed -n '2p' | awk '{print $2}' && nmcli dev show wlp1s0 | grep IP4.ADDRESS | awk '{print $2}' && nmcli dev show wlp1s0 | grep GENERAL.HWADDR | awk '{print $2}'";
+  var commandStr = "nmcli radio wifi && nmcli dev show wlp1s0 | grep GENERAL.HWADDR | awk '{print $2}' && nmcli dev wifi list | grep '*' | sed -n '2p' | awk '{print $2}' && nmcli dev show wlp1s0 | grep IP4.ADDRESS | awk '{print $2}'";
 
   exec(commandStr, env, function(err, stdOut) {
       if (err) {
@@ -13,9 +13,9 @@ function getWifiInfo(config, callback) {
       var data = stdOut.split('\n');
       var wifiInfo = {
         status: data[0],
-        ssid: data[1],
-        address: data[2],
-        MAC: data[3],
+        ssid: data[2],
+        address: data[3],
+        MAC: data[1],
       };
       callback && callback(null, wifiInfo);
   });
