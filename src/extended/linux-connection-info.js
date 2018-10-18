d05b95d62d78fe0bdd8514f039c5904a8c03ae19
Jeff Gnatek <jeff.gnatek@machinemetrics.com>
2018-10-18 16:22:50 -0400

    changes extended linux connection info to work for edge devices

--- src/extended/linux-connection-info.js
+++ src/extended/linux-connection-info.js
@@ -3,43 +3,24 @@ var networkUtils = require('../network-utils');
 var env = require('../env');
 
 function getConnectionInfo(config, connection, callback) {
+  // TODO: This first nmcli command appears to not do anything useful for us in particular? Remove?
+  var commandStr = "nmcli -f ipv4.addresses,ipv4.gateway con show '" + connection +
+    "' | awk '{print $2}' && nmcli -f IP4.ADDRESS,IP4.GATEWAY con show '" + connection +
+    "' | awk '{print $2}'"
 
-  var commandStr = "nmcli -f ipv4.addresses,ipv4.gateway con show " + connection + 
-  " | awk '{print $2}' && nmcli -f IP4.ADDRESS,IP4.GATEWAY con show " + connection + 
-  " | awk '{print $2}'"
+  exec(commandStr, env, function (err, stdOut) {
+    if (err) {
+      callback && callback(err);
+      return;
+    }
 
+    var data = stdOut.split('\n');
 
-  exec(commandStr, env, function(err, stdOut) {
-      if (err) {
-        callback && callback(err);
-        return;
-      }
-      var data = stdOut.split('\n');
-      data.pop();
-      var connectionInfo;
-      if (data.length === 2) {
-        if (data[0] === '') {
-          connectionInfo = {
-            address: '',
-            subnet: '',
-            gateway: ''
-          }
-        } else {
-          connectionInfo = {
-            address: data[0].split('/')[0],
-            subnet: data[0].split('/')[1],
-            gateway: data[1]
-          }
-        }
-      } else if (data.length === 4) {
-        connectionInfo = {
-          address: data[2].split('/')[0],
-          subnet: data[2].split('/')[1],
-          gateway: data[3]
-        }
-      }
-
-      callback && callback(null, connectionInfo);
+    callback && callback(null, {
+      address: data[2].split('/')[0],
+      subnet: data[2].split('/')[1],
+      gateway: data[3]
+    });
   });
 }
 
