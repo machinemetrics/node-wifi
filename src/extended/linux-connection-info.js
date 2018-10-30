9ce2c2cfc11167bb9403a9dc5733de2a8899c170
Jeff Gnatek <jeff.gnatek@machinemetrics.com>
2018-10-30 10:19:59 -0400

    changes way we gather info

--- src/extended/linux-connection-info.js
+++ src/extended/linux-connection-info.js
@@ -1,46 +1,62 @@
-var exec = require('child_process').exec;
-var networkUtils = require('../network-utils');
-var env = require('../env');
+const exec = require('child_process').exec;
+
+const initial = {
+  ipv4: {
+    addresses: null,
+    gateway: null
+  },
+  IP4: {
+    ADDRESS: null,
+    GATEWAY: null
+  }
+};
+
+const commandStr =
+  "nmcli -f ipv4.addresses,ipv4.gateway,IP4.ADDRESS,IP4.GATEWAY con show 'Wired connection 1' | awk '{print $1 $2}'";
 
 function getConnectionInfo(config, connection, callback) {
 
-  var commandStr = "nmcli -f ipv4.addresses,ipv4.gateway con show " + connection + 
-  " | awk '{print $2}' && nmcli -f IP4.ADDRESS,IP4.GATEWAY con show " + connection + 
-  " | awk '{print $2}'"
 
+  exec(commandStr, function (err, stdOut) {
 
-  exec(commandStr, env, function(err, stdOut) {
-      if (err) {
-        callback && callback(err);
-        return;
-      }
-      var data = stdOut.split('\n');
-      data.pop();
-      var connectionInfo;
-      if (data.length === 2) {
-        if (data[0] === '') {
-          connectionInfo = {
-            address: '',
-            subnet: '',
-            gateway: ''
-          }
-        } else {
-          connectionInfo = {
-            address: data[0].split('/')[0],
-            subnet: data[0].split('/')[1],
-            gateway: data[1]
-          }
+    if (err) {
+      callback && callback(err);
+      return;
+    }
+
+    const output = Object.assign({}, initial,
+      stdOut.split('\n').reduce((acc, item) => {
+        const pair = item.split(":");
+        [key, value] = pair;
+        if (key.length) {
+          [parent, child] = key.split(".");
+          acc[parent] = acc[parent] || {};
+          acc[parent][child.indexOf('[') === -1 ? child : child.slice(0, -3)] = value;
         }
-      } else if (data.length === 4) {
-        connectionInfo = {
-          address: data[2].split('/')[0],
-          subnet: data[2].split('/')[1],
-          gateway: data[3]
-        }
-      }
+        return acc;
+      }, {})
+    );
 
-      callback && callback(null, connectionInfo);
+    function splitSubnet(address) {
+      [address, subnet] = address.split('/');
+      return ({
+        address,
+        subnet
+      });
+    }
+
+    const connectionInfo = output.ipv4.addresses.length > 3 ? Object.assign(
+            {},
+            splitSubnet(output.ipv4.addresses),
+            { gateway: output.ipv4.gateway }
+          ) : Object.assign({}, splitSubnet(output.IP4.ADDRESS), {
+            gateway: output.IP4.GATEWAY
+          });
+
+    callback && callback(null, connectionInfo);
+
   });
+
 }
 
 module.exports = function (config) {
